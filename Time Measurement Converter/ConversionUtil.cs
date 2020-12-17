2705325f06d219deebd94131b8232291a025d889
JJRoot822 <josh.root822@gmail.com>
2020-12-17 02:38:58 -0600

    Initial Commit

---
+++ Time Measurement Converter/ConversionUtil.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Time_Measurement_Converter
+{
+    class ConversionUtil
+    {
+        public static double convertMilisecondsToSeconds(double miliseconds) { }
+        public static double convertMilisecondsToMinutes(double miliseconds) { }
+        public static double convertMilisecondsToHour(double miliseconds) { }
+        public static double convertMilisecondsToDay(double miliseconds) { }
+        public static double convertMilisecondsToWeek(double miliseconds) { }
+        public static double convertMilisecondsToMonth(double miliseconds) { }
+        public static double convertMilisecondsToYear(double miliseconds) { }
+
+
+        public static double convertSecondsToMiliseconds(double seconds) { }
+        public static double convertSecondsToMinutes(double seconds) { }
+        public static double convertSecondstoHours(double seconds) { }
+        public static double convertSecondsToDays(double seconds) { }
+        public static double convertSecondsToWeeks(double seconds) { }
+        public static double convertSecondsToMonths(double seconds) { }
+        public static double convertSecondsToYears(double seconds) { }
+
+
+        public static double convertMinutesToMiliseconds(double minutes) { }
+        public static double convertMinutesToSeconds(double minutes) { }
+        public static double convertMinutesToHours(double minutes) { }
+        public static double convertMinutesToDays(double minutes) { }
+        public static double convertMinutesToWeeks(double minutes) { }
+        public static double convertMinutesToMonths(double minutes) { }
+        public static double convertMinutesToYears(double minutes) { }
+
+
+        public static double convertHoursToMiliseconds(double hours) { }
+        public static double convertHoursToSeconds(double hours) { }
+        public static double convertHoursToMinutes(double hours) { }
+        public static double convertHoursToDays(double hours) { }
+        public static double convertHoursToWeeks(double hours) { }
+        public static double convertHoursToMonths(double hours) { }
+        public static double convertHoursToYears(double hours) { }
+
+
+        public static double convertDaysToMiliseconds(double days) { }
+        public static double convertDaysToSeconds(double days) { }
+        public static double convertDaysToMinutes(double days) { }
+        public static double convertDaysToHours(double days) { }
+        public static double convertDaysToWeeks(double days) { }
+        public static double convertDaysToMonths(double days) { }
+        public static double convertDaysToYears(double days) { }
+
+
+        public static double convertWeeksToMiliseconds(double weeks) { }
+        public static double convertWeeksToSeconds(double weeks) { }
+        public static double convertWeeksToMinutes(double weeks) { }
+        public static double convertWeeksToHours(double weeks) { }
+        public static double convertWeeksToDays(double weeks) { }
+        public static double convertWeeksToMonths(double weeks) { }
+        public static double convertWeeksToYears(double weeks) { }
+
+
+        public static double convertMonthsToMiliseconds(double months) { }
+        public static double convertMonthsToSeconds(double months) { }
+        public static double convertMonthsToMinutes(double months) { }
+        public static double convertMonthsToHours(double months) { }
+        public static double convertMonthsToDays(double months) { }
+        public static double convertMonthsToWeeks(double months) { }
+        public static double convertMonthsToYears(double months) { }
+
+
+        public static double convertYearsToMiliseconds(double years) { }
+        public static double convertYearsToSeconds(double years) { }
+        public static double convertYearsToMinutes(double years) { }
+        public static double convertYearsToHours(double years) { }
+        public static double convertYearsToDays(double years) { }
+        public static double convertYearsToWeeks(double years) { }
+        public static double convertYearsToMonths(double years) { }
+    }
+}
