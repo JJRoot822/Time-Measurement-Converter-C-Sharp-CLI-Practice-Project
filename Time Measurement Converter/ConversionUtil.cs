e73455f3a0430a61ae422b137c6c52ae5b282972
JJRoot822 <josh.root822@gmail.com>
2020-12-17 18:24:23 -0600

    Fixed Conversion Calculations and removed testing Console.WriteLine() statements.

--- Time Measurement Converter/ConversionUtil.cs
+++ Time Measurement Converter/ConversionUtil.cs
@@ -16,9 +16,9 @@ namespace Time_Measurement_Converter
 
 
         public static double ConvertSecondsToMiliseconds(double seconds) { return seconds * 1_000; }
-        public static double ConvertSecondsToMinutes(double seconds) { return seconds / 60); }
-        public static double ConvertSecondstoHours(double seconds) { return seconds / 3_600; }
-        public static double ConvertSecondsToDays(double seconds) { return seconds / 86_400); }
+        public static double ConvertSecondsToMinutes(double seconds) { return seconds / 60; }
+        public static double ConvertSecondsToHours(double seconds) { return seconds / 3_600; }
+        public static double ConvertSecondsToDays(double seconds) { return seconds / 86_400; }
         public static double ConvertSecondsToWeeks(double seconds) { return seconds / 604_800; }
         public static double ConvertSecondsToMonths(double seconds) { return seconds / 2_629_746; }
         public static double ConvertSecondsToYears(double seconds) { return seconds / 31_556_952; }
@@ -30,7 +30,7 @@ namespace Time_Measurement_Converter
         public static double ConvertMinutesToDays(double minutes) { return minutes / 1_440; }
         public static double ConvertMinutesToWeeks(double minutes) { return minutes / 10_080; }
         public static double ConvertMinutesToMonths(double minutes) { return minutes / 43_829.1; }
-        public static double ConvertMinutesToYears(double minutes) { return minutes / 525_949.2; }
+        public static double ConvertMinutesToYears(double minutes) { return minutes / 525_949.2; } 
 
 
         public static double ConvertHoursToMiliseconds(double hours) { return hours * 3_600_000; }
@@ -60,19 +60,19 @@ namespace Time_Measurement_Converter
         public static double ConvertWeeksToYears(double weeks) { return weeks / 52.18; }
         
 
-        public static double ConvertMonthsToMiliseconds(double months) { return months * 2_629_746_000 }
-        public static double ConvertMonthsToSeconds(double months) { return months * 2_629_745; }
+        public static double ConvertMonthsToMiliseconds(double months) { return months * 2_629_746_000; }
+        public static double ConvertMonthsToSeconds(double months) { return months * 2_629_746; }
         public static double ConvertMonthsToMinutes(double months) { return months * 43_829.1; }
         public static double ConvertMonthsToHours(double months) { return months * 730.49; }
         public static double ConvertMonthsToDays(double months) { return months * 31; }
         public static double ConvertMonthsToWeeks(double months) { return months * 52.18; }
-        public static double ConvertMonthsToYears(double months) { return months / 365; }
+        public static double ConvertMonthsToYears(double months) { return months / 12; }
 
 
         public static double ConvertYearsToMiliseconds(double years) { return years * 31_556_952_000; }
-        public static double ConvertYearsToSeconds(double years) { return years * 31_556_952 }
+        public static double ConvertYearsToSeconds(double years) { return years * 31_556_952; }
         public static double ConvertYearsToMinutes(double years) { return years * 525_949.2; }
-        public static double ConvertYearsToHours(double years) { return years * 8_765.82 }
+        public static double ConvertYearsToHours(double years) { return years * 8_765.82; }
         public static double ConvertYearsToDays(double years) { return years * 365; }
         public static double ConvertYearsToWeeks(double years) { return years * 52.18; }
         public static double ConvertYearsToMonths(double years) { return years * 12; }
